938f77f873e96e33222d272b055a056c140af5d0
Qiuyu Huang <huangq4@outlook.com>
2025-01-21 10:38:27 -0500

    configure formatter with conform

--- .config/nvim/lua/redacted24/plugins/conform.lua
+++ .config/nvim/lua/redacted24/plugins/conform.lua
@@ -1,16 +1,17 @@
 return {
-    'stevearc/conform.nvim',
-    opts = {
-        formatters_by_ft = {
-            java = {"google-java-format"}
-            -- Conform will run multiple formatters sequentially
-            -- python = { "isort", "black" },
-            -- Conform will run the first available formatter
-            -- javascript = { "prettierd", "prettier", stop_after_first = true },
-        },
-        -- format_on_save = {
-        --     timeout_ms = 500,
-        --     lsp_format = "fallback",
-        -- },
-    },
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			java = { "clang-format" },
+			-- Conform will run multiple formatters sequentially
+			-- python = { "isort", "black" },
+			-- Conform will run the first available formatter
+			-- javascript = { "prettier", "prettier", stop_after_first = true },
+		},
+
+		format_on_save = function(bufnr)
+			return { timeout_ms = 500, lsp_format = "fallback" }
+		end,
+	},
 }
